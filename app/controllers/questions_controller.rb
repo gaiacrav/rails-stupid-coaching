b424b83fd99037ab2e7acbeb1cb3b3e3344064dc
Gaia Cravesana <gcravesana@gmail.com>
2021-02-15 15:12:29 +0000

    fixed the controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,25 +1,15 @@
 class QuestionsController < ApplicationController
 
   def ask
-    @question = ""
   end
 
   def answer
-    @answer = answer
-  end
-
-  def coachAnswer
     if @question == "I am going to work"
-      puts "Great!"
-    elsif isAquestion?
-      puts "Silly question, get dressed and go to work!"
+      @answer == "Great!"
+    elsif @question[-1] == "?"
+      @answer == "Silly question, get dressed and go to work!"
     else
-      puts "I don't care, get dressed and go to work!"
+      @answer == "I don't care, get dressed and go to work!"
     end
   end
-
-  def isAquestion?
-   @question.end_with?("?")
-  end
-
 end
